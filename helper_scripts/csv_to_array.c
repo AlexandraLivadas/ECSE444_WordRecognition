5dd9c2b5f1b08bae2389754cac5a00ece52dcf4c
alexandra-livadas <alexandra.livadas@mail.mcgill.ca>
2020-04-21 18:36:19 -0400

    Changes made to MFCC computation method

--- helper_scripts/csv_to_array.c
+++ helper_scripts/csv_to_array.c
@@ -12,27 +12,28 @@
 
 //fftw_complex *out;
 double *out;
+double *mfcc[500][12];
+double *mfcc_result;
 fftw_plan p;
 
 
 double *computeFFT(double* input){
     FILE *f_fft;
     f_fft = fopen("../data/FFT/one_Alex1_fft", "wb"); 
-    int n = N+1;
     //out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * (n/2+1));
-    out = (double*) malloc(sizeof(double)*n);
+    out = (double*) malloc(sizeof(double)*N);
     //in = (double*)input;
     //p = fftw_plan_dft_r2c_1d(n, input, out, FFTW_ESTIMATE);
     //This is a real-to-real FFT with a half-complex format???
-    p = fftw_plan_r2r_1d(n, input, out, FFTW_R2HC, FFTW_ESTIMATE);
+    p = fftw_plan_r2r_1d(N, input, out, FFTW_R2HC, FFTW_ESTIMATE);
     for (int i = 0; i < N; i++) 
         input[i] = input[i]/512; //idk
 
     fftw_execute(p); /* repeat as needed */
 
-    for (int i = 0; i < n; i++){
-        fprintf(f_fft, "%lf, ", out[i]);
-        printf("%lf, ", out[i]);
+    for (int i = 0; i < N; i++){
+        fprintf(f_fft, "%lf\n", out[i]);
+        //printf("%lf, ", out[i]);
     }
 
     return out;
@@ -54,46 +55,58 @@ double *csv_to_arr(char *filename){
     }
 
     while (fscanf(fp, "%lf,", &a[count]) != EOF) {
-        printf("%lf, ", a[count]);
+        //printf("%lf, ", a[count]);
         count++;
     }
-    printf("\n Count: %zu\n", count);
+    //printf("\n Count: %zu\n", count);
 
     fclose(fp);
 
     return a;
 }
 
-double *computeMFCC(double *in, int n){
-    FILE *f;
-    f = fopen("../data/MFCC/one_Alex1_mfcc", "wb");
-    double *mfcc_result = (double *) malloc(sizeof(double)*n);
+double *computeMFCC(double *in, int n, int frame_n, FILE *f){
+    //double *mfcc_result = (double*) malloc(sizeof(double)*n);
+mfcc_result = (double*) malloc(sizeof(double)*n);
 
-    if (f == NULL){
-        printf("Error: file does not exist");
-        exit(1);
-    }
     //Compute the first n coefficients
 	for(int coeff = 0; coeff < n; coeff++)
 	{
-		mfcc_result[coeff] = GetCoefficient(in, 16000, 48, 182, coeff);
-        fprintf(f, "%i: %f, og: %lf\n", coeff, mfcc_result[coeff], in[coeff]);
-		printf("%i %f, ", coeff, mfcc_result[coeff]);
+        //If on frame 3 and coeff 4, we would want to calculate the MFCC @ (2*12)+4 (pretty sure)
+        int i = coeff+(n*frame_n);
+		mfcc_result[coeff] = GetCoefficient(in, 16000, 48, 182, i); 
+        fprintf(f, "%i: %f, og: %lf\n", i, mfcc_result[coeff], in[i]);
+		//printf("%i %f, ", i, mfcc_result[coeff]);
 	}
-    fclose(f);
+
     return mfcc_result;
 }
 
-int main(void) {
+int main(void) { 
     double *a = (double*) malloc(sizeof(double)*N);
     a = csv_to_arr("../data/original/one_Alex1.csv");
 
+    // double *i = (double*) malloc(sizeof(double)*(N+1));
+    // i = computeFFT(a);
     computeFFT(a);
 
     fftw_destroy_plan(p);
 
-    double *mfcc = (double*) malloc(sizeof(double)*(N+1));
-    mfcc = computeMFCC(out, 4096);
+    FILE *f;
+    f = fopen("../data/MFCC/one_Alex1_mfcc", "wb");
+    //For 6000 inputs, 12 MFCC per frame: 500 frames
+    //double mfcc[500][12];
+    for (int j = 0; j < 10; j++){
+        fprintf(f, "FRAME NUMBER: %d\n", j);
+        double *m = computeMFCC(out, 12, j, f);
+        memcpy(mfcc[j], m, sizeof(int) * 12);
+    }
+
+    for (int k = 0; k < 10; k++){
+        for (int m = 0; m < 12; m++){
+            printf("%lf, ", mfcc[k][m]);
+        }
+    }
     
     return 0;
 }
